b1c710f19427a363cd618752c327fe50d38c771b
PhuongDong <phuongdong2097@gmail.com>
2021-12-25 23:31:13 +0700

    add component tutorial

---
+++ src/app/tutorial.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-tutorial',
+    template: '<h2>This is Tedu Angular 2 tutorial</h2>'
+})
+
+export class TutorialComponent{}
