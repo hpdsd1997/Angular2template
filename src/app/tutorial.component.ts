14ae76f990b47df9a7ff30625ecd08ade6a82c19
PhuongDong <phuongdong2097@gmail.com>
2021-12-26 10:32:52 +0700

    leeson 7

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,8 +1,12 @@
+import { style } from '@angular/animations';
 import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
-    template: '<h2>This is Tedu Angular 2 tutorial</h2>'
+    template: `<h2>This is Tedu Angular 2 tutorial</h2>
+        <h4>Angular Component</h4>
+    `,
+    styles:['h4 {color:red}']
 })
 
 export class TutorialComponent{}
