b7a653b524280665ce0988a98d1ce0bdf1c55b1a
PhuongDong <phuongdong2097@gmail.com>
2021-12-27 21:26:10 +0700

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
-
+  templateUrl: 'app.component.html',
+  styleUrls:['app.component.css']
 })
 export class AppComponent {
   onSubmit(value: any) {
     console.log(value);
   }
 
+  public cities=[{Id:1,Name:"Ha Noi"},{Id:2,Name:"HCM"}];
 }
