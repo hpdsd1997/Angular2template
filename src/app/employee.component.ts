2b9635da1716f9c9ae4c3308e6b36a2acf6956de
PhuongDong <phuongdong2097@gmail.com>
2021-12-29 23:00:49 +0700

    query params

--- src/app/employee.component.ts
+++ src/app/employee.component.ts
@@ -1,5 +1,6 @@
 import { EmployeeService } from "./service/employee.service";
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 @Component({
     selector: 'employee-list',
     templateUrl: 'employee.component.html'
@@ -7,16 +8,25 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EmployeeListComponent implements OnInit {
     employees: any[];
-    constructor(private employeeService: EmployeeService) {
-
+    public pages: number[];
+    public currentPage: number;
+    constructor(private employeeService: EmployeeService, private router: Router, private activatedRouter: ActivatedRoute) {
     }
+
     ngOnInit(): void {
+        this.activatedRouter.queryParams.subscribe(params => {
+            this.currentPage = params['numberPage'] || 1;
+            console.log(this.currentPage);
+            console.log(params['filter']);
+        });
+
         this.employeeService.GetList().subscribe((response: any) => {
             this.employees = response;
         },
-        error=>{
-            console.log("System error API");
-        }
+            error => {
+                console.log("System error API");
+            }
         );
+        this.pages = [1, 2, 3, 4, 5];
     }
 }
